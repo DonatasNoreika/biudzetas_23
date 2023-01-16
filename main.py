250351bac033171fbcfa32ba14e75007da1e20c8
Donatas Noreika <donoras@gmail.com>
2023-01-16 20:41:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from modules import Irasas
+
+zurnalas = []
+
+while True:
+    pasirinkimas = int(input("1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - atvaizduoti žurnalą\n0 - išeiti\n"))
+    match pasirinkimas:
+        case 1:
+            suma = int(input("Įveskite sumą: "))
+            irasas = Irasas(suma, "Pajamos")
+            zurnalas.append(irasas)
+        case 2:
+            suma = int(input("Įveskite sumą: "))
+            irasas = Irasas(suma, "Išlaidos")
+            zurnalas.append(irasas)
+        case 3:
+            print(zurnalas)
+        case 0:
+            print("Viso gero")
+            break
