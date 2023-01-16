59c603589f9a9638c44598807a94d2eea1621aae
Donatas Noreika <donoras@gmail.com>
2023-01-16 21:14:00 +0200

    +Leisti išsaugoti žurnalą (pickle)

--- modules.py
+++ modules.py
@@ -1,7 +1,27 @@
 class Irasas:
-    def __init__(self, suma, tipas):
+    def __init__(self, suma):
         self.suma = suma
-        self.tipas = tipas
 
     def __repr__(self):
-        return f"{self.tipas}: {self.suma}"
+        return f"{self.suma}"
+
+
+class PajamuIrasas(Irasas):
+    def __init__(self, suma, siuntejas, info):
+        super().__init__(suma)
+        self.siuntejas = siuntejas
+        self.info = info
+
+    def __repr__(self):
+        return f"Pajamos: {self.suma} (siuntėjas - {self.siuntejas}, info - {self.info})"
+
+
+class IslaiduIrasas(Irasas):
+    def __init__(self, suma, budas, isigyta, info):
+        super().__init__(suma)
+        self.budas = budas
+        self.isigyta = isigyta
+        self.info = info
+
+    def __repr__(self):
+        return f"Išlaidos: {self.suma} (būdas - {self.budas}, įsigyta - {self.isigyta}, info - {self.info})"
